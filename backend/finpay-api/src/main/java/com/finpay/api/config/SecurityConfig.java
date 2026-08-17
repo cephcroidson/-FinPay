436bc17b6ebbfb039a44e1eb31cd04226e0d3e36
Cephus Ongata <cephcroidson@gmail.com>
2026-08-17 15:39:16 +0300

    Harden JWT security configuration

--- backend/finpay-api/src/main/java/com/finpay/api/config/SecurityConfig.java
+++ backend/finpay-api/src/main/java/com/finpay/api/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.finpay.api.config;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 import com.finpay.api.security.JwtAuthenticationFilter;
 import com.finpay.api.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
@@ -54,13 +57,45 @@ public class SecurityConfig {
 
         return configuration.getAuthenticationManager();
     }
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(
+            List.of("http://localhost:5173")
+    );
+
+    configuration.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    );
+
+    configuration.setAllowedHeaders(
+            List.of("*")
+    );
+
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration(
+            "/**",
+            configuration
+    );
+
+    return source;
+}
 
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception {
+       http
+        .csrf(AbstractHttpConfigurer::disable)
 
-        http
-                .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors
+                .configurationSource(corsConfigurationSource())
+        )
 
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(
